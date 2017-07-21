411305a048967d09976d411d1f2a02ff1030b215
Federico Hoerth <fhoerth@toolboxtve.com>
2017-07-21 16:27:05 -0300

    babel-polyfill and configureStore for dev and prod

--- app/index.js
+++ app/index.js
@@ -1,18 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { AppContainer } from 'react-hot-loader'
-
+import 'babel-polyfill'
 import './styles/App.scss'
 
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { AppContainer } from 'react-hot-loader'
+
 import App from './App'
+import configureStore from './store/configureStore'
 
+const store = configureStore()
 const rootEl = document.getElementById('root')
-const render = Component => ReactDOM.render(
-  <AppContainer>
-    <Component />
-  </AppContainer>,
-  rootEl
-)
 
-render(App)
-if (module.hot) module.hot.accept('./App', () => render(App))
+if (process.env.NODE_ENV === 'production') {
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootEl
+  )
+} else {
+  const DevApp = AppComponent => (
+    <AppContainer>
+      <Provider store={store}>
+        <AppComponent />
+      </Provider>
+    </AppContainer>
+  )
+  const SuperApp = DevApp(App)
+
+  const appInstance = ReactDOM.render(SuperApp, rootEl)
+
+  if (module.hot) {
+    const reactDeepForceUpdate = require('react-deep-force-update')
+    module.hot.accept('./App', () => {
+      reactDeepForceUpdate(appInstance)
+    })
+  }
+}
